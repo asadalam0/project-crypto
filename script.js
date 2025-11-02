45fe3c7cfad962e2a1c3db8aed424c76e3ec6f2b
Mohammad Asad Alam <148976906+asadalam0@users.noreply.github.com>
2025-11-03 01:34:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ form.addEventListener("submit", async (e) => {
   const password = document.getElementById("password").value;
 
   try {
-    const response = await fetch("https://api.globerise.eu/api/v1/auth/login", {
+    const response = await fetch("https://api.globerise.eu/api/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
@@ -37,3 +37,4 @@ form.addEventListener("submit", async (e) => {
     message.textContent = "Something went wrong.";
   }
 });
+

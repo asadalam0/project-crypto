37c2c4837451cdc4fd507dfc84a6f9c8e820f767
Mohammad Asad Alam <148976906+asadalam0@users.noreply.github.com>
2025-11-03 01:42:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ form.addEventListener("submit", async (e) => {
   const password = document.getElementById("password").value;
 
   try {
-    const response = await fetch("https://api.globerise.eu/api/auth/login", {
+    const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.globerise.eu/api/v1/auth/login', {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
@@ -37,4 +37,5 @@ form.addEventListener("submit", async (e) => {
     message.textContent = "Something went wrong.";
   }
 });
+
 
